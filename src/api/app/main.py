0b697b7f5ae902ad795142d0ced8bbb872476782
EC2 Default User <ec2-user@ip-172-31-75-109.ec2.internal>
2018-05-19 01:58:54 +0000

    Update

--- src/api/app/main.py
+++ src/api/app/main.py
@@ -1,12 +1,13 @@
 from flask import Flask, jsonify
-import sys
+# import sys
+import os
 
 # the all-important app variable:
 app = Flask(__name__)
 
 @app.route('/v1/uploader', methods=['POST'])
 def uploader():
-    return jsonify({'value':5})
+    return jsonify({'value':os.getcwd()})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True, port=80) # Webserver
