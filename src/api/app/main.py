edeae6c7641037a6beba40b1d015ef02f388265e
EC2 Default User <ec2-user@ip-172-31-75-109.ec2.internal>
2018-05-19 20:28:17 +0000

    Update

--- src/api/app/main.py
+++ src/api/app/main.py
@@ -1,8 +1,6 @@
 from flask import Flask, request, jsonify
 from werkzeug import secure_filename
 import boto3
-# from OpenSSL import SSL
-
 
 # the all-important app variable:
 app = Flask(__name__)
@@ -24,8 +22,5 @@ def test():
     return jsonify({'test':1})
 
 if __name__ == '__main__':
-    # context = SSL.Context(SSL.SSLv23_METHOD)
-    # context.use_privatekey_file('ssl.key')
-    # context.use_certificate_file('ssl.cert')
-    app.run(host='0.0.0.0', debug=True, port=5000) # Webserver
+    app.run(host='0.0.0.0', debug=True, port=80) # Webserver
     # app.run(host='127.0.0.1', debug=True, port=8080) # Localhost (Cloud 9)
